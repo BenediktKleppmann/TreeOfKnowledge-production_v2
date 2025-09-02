8720914f491eff9e2cc509709469d8a2b45b434c
BenediktKleppmann <benedikt@kleppmann.de>
2025-09-02 16:21:14 +0200

    the system has made massive changes to over 4000 files. It however still works both locally and on AWS

---
+++ eb-env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Ben\Documents\6 github repositories\TreeOfKnowledge-production_v2\eb-env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
