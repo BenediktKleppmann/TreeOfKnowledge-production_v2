884f0f96a3bfbe57df802e267e7d2cf2aeccee8a
BenediktKleppmann <benedikt@kleppmann.de>
2025-10-31 17:28:29 +0100

    removing requirement for mysql python library

--- webservice/settings_production.py
+++ webservice/settings_production.py
@@ -11,48 +11,52 @@
 # Inherit from standard settings file for default
 from webservice.settings import *
 import os
+import dj_database_url
 
 # Everything below will override our standard settings:
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-
+# ----- DATABASES -----
 if 'RDS_HOSTNAME' in os.environ:
+    # Your EB RDS is POSTGRES, not MySQL
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'PORT': os.environ.get('RDS_PORT', '5432'),
         }
     }
+elif os.getenv('DATABASE_URL'):
+    # fallback: parse DATABASE_URL if you ever set it
+    DATABASES['default'] = dj_database_url.config()
 else:
-	DATABASES['default'] = dj_database_url.config(default=None)
+    # last resort: local sqlite so the app can still boot
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 DB_CONNECTION_URL = DB_CONNECTION_URL = os.getenv('DATABASE_URL', 'awseb-e-jivwhga8ui-stack-awsebrdsdatabase-whgz4rmetb10.cee9izytbdnd.eu-central-1.rds.amazonaws.com')
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 USE_X_FORWARDED_HOST = True
-
-# Allow all host headers
-ALLOWED_HOSTS  = ['*', 'Treeofknowledge-production-5.eba-ffmsq3fy.eu-central-1.elasticbeanstalk.com']
-
-# Set debug to False
+ALLOWED_HOSTS  = ['*', 'www.treeofknowledge.ai', 'treeofknowledge.ai', 'Treeofknowledge-production-5.eba-ffmsq3fy.eu-central-1.elasticbeanstalk.com']
 DEBUG = False
 
 # ----- middleware: prepend whitenoise, don't replace -----
-MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-] + MIDDLEWARE  # use the list from base settings
+MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware'] + MIDDLEWARE  # use the list from base settings
 
 
 # ----- static -----
 STATIC_URL = '/static/'
 STATIC_ROOT = '/var/app/current/static/'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # <-this is super strict: it will make 'eb deploy' fail if there is a reference to a static file that no longer is in the right folder
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # <-this is super strict: it will make 'eb deploy' fail if there is a reference to a static file that no longer is in the right folder
+
 
 
